c87b6b30d19f4b122d8cb3b420a5ba8a425cc9a5
Agha Syed Nasir Mahmood Azeemi <54790791+nasir-azeemi@users.noreply.github.com>
2022-08-17 17:51:14 +0500

    Update MyOrders.js

--- Ecommerce-App/screens/MyOrders.js
+++ Ecommerce-App/screens/MyOrders.js
@@ -7,12 +7,16 @@ import {
   SafeAreaView,
 } from "react-native";
 
+import { useOrder } from "../providers/OrderProvider.js";
+
 import OrderItemUser from "../components/Items/OrderItemUser.js";
 
 import UniversalStyles from "../styles/UniversalStyles.js";
 import IonIcon from "react-native-vector-icons/Ionicons";
 
 export default function MyOrders({ navigation }) {
+  const { orders } = useOrder();
+
   return (
     <SafeAreaView style={UniversalStyles.page_container}>
       <View style={UniversalStyles.page_container}>
@@ -25,7 +29,7 @@ export default function MyOrders({ navigation }) {
             <Pressable onPress={() => navigation.goBack()}>
               <IonIcon name="arrow-back" size={30} color="grey" />
             </Pressable>
-            <Text style={{ fontSize: 23 }}>My Orders</Text>
+            <Text style={{ fontSize: 23 }}>My Orders ({orders.length})</Text>
           </View>
 
           <OrderItemUser />
